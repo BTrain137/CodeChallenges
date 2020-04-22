1759ca3ef1a0365eabf4d23bb0786801d30f2edc
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-04-21 20:54:18 -0700

    Example test file

---
+++ basic.test.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect;
+const func = require("./app");
+
+//Point to exact folder
+//npm run mocha ./
+describe("func", () => {
+  it("", () => {
+    expect(func("")).to.equal("");
+  });
+});
