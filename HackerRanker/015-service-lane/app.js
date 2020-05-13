fbd5e5f3fe85cd55362fa0eecd7d16ea2bce8678
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-05-13 09:32:46 -0700

    formatting

--- HackerRanker/015-service-lane/app.js
+++ HackerRanker/015-service-lane/app.js
@@ -28,30 +28,34 @@ function readLine() {
 
 // Complete the serviceLane function below.
 function serviceLane(width, cases) {
-  return cases.map(lane => {
+  return cases.map((lane) => {
     const arr = [];
     for (let i = lane[0]; i <= lane[1]; i++) {
       arr.push(parseInt(width[i]));
     }
-    return Math.min(...arr.map(num => num));
+    return Math.min(...arr.map((num) => num));
   });
 }
 
 function main() {
   const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-  const nt = readLine().split(' ');
+  const nt = readLine().split(" ");
 
   const n = parseInt(nt[0], 10);
 
   const t = parseInt(nt[1], 10);
 
-  const width = readLine().split(' ').map(widthTemp => parseInt(widthTemp, 10));
+  const width = readLine()
+    .split(" ")
+    .map((widthTemp) => parseInt(widthTemp, 10));
 
   let cases = Array(t);
 
   for (let i = 0; i < t; i++) {
-      cases[i] = readLine().split(' ').map(casesTemp => parseInt(casesTemp, 10));
+    cases[i] = readLine()
+      .split(" ")
+      .map((casesTemp) => parseInt(casesTemp, 10));
   }
 
   let result = serviceLane(width, cases);
