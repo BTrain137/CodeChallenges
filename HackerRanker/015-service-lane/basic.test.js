b38c6d43301000383bb863a75ea97a281455d2b1
bryan89tran@yahoo.com <bryan89tran@yahoo.com>
2020-05-13 09:29:42 -0700

    learned how to work with hanker ranker code

---
+++ HackerRanker/015-service-lane/basic.test.js
@@ -0,0 +1,14 @@
+const expect = require("chai").expect;
+const serviceLane = require("./app");
+
+//Point to exact folder
+//npm run mocha ./HackerRanker/015-serviceLane
+describe("serviceLane", () => {
+  it("", () => {
+    expect(serviceLane()).to.deep.equal();
+  });
+
+  it("", () => {
+    expect(serviceLane()).to.deep.equal();
+  });
+});
